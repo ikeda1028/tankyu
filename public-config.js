e6fadeae99458e3843183d163ce48a835dd3ebdf
ikeda1028 <ikeda1028@users.noreply.github.com>
2026-06-23 14:37:34 +0900

    Configure public Firebase settings

--- public-config.js
+++ public-config.js
@@ -2,5 +2,12 @@ window.WAKUWAKU_CONFIG = {
   googleMapsApiKey: "",
   driveApiUrl: "",
   adminEmails: ["ikeda@manabinomichi.com"],
-  firebase: {},
+  firebase: {
+    apiKey: "AIzaSyBBgU_a3GG-5LOIjbcSkMZ93XQuPALZV4A",
+    authDomain: "tankyu-723fc.firebaseapp.com",
+    projectId: "tankyu-723fc",
+    storageBucket: "tankyu-723fc.firebasestorage.app",
+    messagingSenderId: "318168393711",
+    appId: "1:318168393711:web:9d0ea96b0511f6a5cf381a",
+  },
 };
